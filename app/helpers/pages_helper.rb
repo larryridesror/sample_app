3e1f6d1ebd886584e3c32b0426f942e498302af5
larry <larry@guffin.de>
2011-07-27 13:06:36 +0200

    Done with sign in

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,16 +1,2 @@
 module PagesHelper
-
-  # Return a title on a per-page basis
-  def title
-    base_title = "Ruby on Rails Tutorial"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
-  end
-
-  def logo
-    logo = image_tag "logo.png", :alt => "Sample App", :class => "round"
-  end
 end
